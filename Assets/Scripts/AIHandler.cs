460b5c1d52ee4ea17626f014932fcfef7386d316
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-20 15:58:40 +1100

    class update

---
+++ Assets/Scripts/AIHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIHandler : MonoBehaviour
+{
+    public int pathIndex;
+    public enum Path
+    {
+        Path1, Path2
+    }
+    public Path myPath;
+    public Transform[] wayPoints;
+    // Use this for initialization
+    void Start()
+    {
+        pathIndex = Random.Range(0, 2);
+        myPath = (Path)pathIndex;
+        if(myPath == Path.Path1)
+        {
+            wayPoints = GameObject.FindGameObjectWithTag("WPH").GetComponent<PathHandler>().wayPointPath1;
+        }
+        else
+        {
+            wayPoints = GameObject.FindGameObjectWithTag("WPH").GetComponent<PathHandler>().wayPointPath2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PathHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathHandler : MonoBehaviour
+{
+    public Transform[] wayPointPath1, wayPointPath2;
+	
+}
