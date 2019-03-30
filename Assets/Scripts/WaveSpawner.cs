6f21e0fb17fa13d70fdaaa357fe0b507aabd81d1
irlartboy <ollie_king99@outlook.com>
2019-03-30 14:24:50 +1100

    Enemy Spawner added
    enemy spawner added - not functioning yet

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform knightPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 3f;
+    private float countdown = 2f;
+
+    private int waveIndex = 0;
+    private void Update()
+    {
+         if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+
+    }
+    IEnumerator SpawnWave ()
+    {
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnKnight();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveIndex++;
+        //Debug.Log("Wave Incoming!");
+    }
+    void SpawnKnight()
+    {
+        Instantiate(knightPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
